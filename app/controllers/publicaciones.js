8d4d561a0dd7952cc113ca80462d9798bc58608e
Emiliano Conti <econtirodriguez@outlook.com>
2015-02-17 13:19:27 -0200

    lalalala

--- app/controllers/publicaciones.js
+++ app/controllers/publicaciones.js
@@ -129,9 +129,12 @@ function VerPublicacion(evt){
 	var pubs = $.coleccionPublicaciones.toJSON();
 	var p = _.where(pubs,{IdPublicacion:id})[0];
 	//TODO: IR AL CONTROLADOR DE LA PUBLICACION
-	Alloy.createController("publicacion",{
+	Alloy.Globals.AbrirVentana("publicacion",{
 		"Publicacion":p
-	}).getView().open();
+	});
+	/*Alloy.createController("publicacion",{
+		"Publicacion":p
+	}).getView().open();*/
 }
 
 
